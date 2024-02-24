a2b99305ef95a8fd9ca33ef7e3a8965281a83466
Yogesh Singh <110375635+yogesh16singh@users.noreply.github.com>
2024-02-24 14:17:05 +0000

    feat: Added NextAuth, Upstash Redis Adapter, React Hot Toast, and Providers component and fixed layout in components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import Button from "@/components/ui/Button";
-export default function Home() {
+
+export default async function Home() {
   return (
     <Button variant="default" size="sm">
       Button
